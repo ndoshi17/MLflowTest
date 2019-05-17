11cf1699a0c3710faf55a9c12df4091622926cca
Nihar Doshi <ndoshi@ip-10-2-44-175.us-west-2.compute.internal>
2019-05-17 15:10:34 -0700

    with run

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import mlflow
 from mlflow import log_metric, log_param, log_artifact
 
 
@@ -7,12 +8,14 @@ from mlflow import log_metric, log_param, log_artifact
 def main():
     param1 = sys.argv[0]
     # Log a parameter (key-value pair)
-    log_param("param1", param1)
+    with mlflow.start_run():
+
+        log_param("param1", param1)
 
     # Log a metric; metrics can be updated throughout the run
-    log_metric("foo", 1)
-    log_metric("foo", 2)
-    log_metric("foo", 3)
+        log_metric("foo", 1)
+        log_metric("foo", 2)
+        log_metric("foo", 3)
 
     # Log an artifact (output file)
     #with open("output.txt", "w") as f:
